c19e9726aacfde31390b9707b4935373bd1b5721
MiguelElGallo <60221874+MiguelElGallo@users.noreply.github.com>
2025-05-11 12:30:29 +0300

    Add initial configuration files for Snowflake deployment with Tofu

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # snowtofu
 Deploy Snowflake objects from Tofu (Open Source terraform) 
+
+
+##
+shell
+$ openssl genrsa 2048 | openssl pkcs8 -topk8 -inform PEM -out snowflake_tf_snow_key.p8 -nocrypt
+$ openssl rsa -in snowflake_tf_snow_key.p8 -pubout -out snowflake_tf_snow_key.pub
+
+
+Copy the text contents of the ~/.ssh/snowflake_tf_snow_key.pub file, including the PUBLIC KEY header and the PUBLIC KEY footer.
+
+sql
+USE ROLE ACCOUNTADMIN;
+
+CREATE USER TERRAFORM_SVC
+    TYPE = SERVICE
+    COMMENT = "Service user for Terraforming Snowflake"
+    RSA_PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----
+
+-----END PUBLIC KEY-----";
+
+GRANT ROLE SYSADMIN TO USER TERRAFORM_SVC;
+GRANT ROLE SECURITYADMIN TO USER TERRAFORM_SVC;
+
+
+
+Go to your GitHub repository.
+Click on "Settings".
+In the left sidebar, under "Security", click on "Secrets and variables", then "Actions".
+Click on "New repository secret" for each of the secrets you need to create:
+TF_VAR_ORGANIZATION_NAME: Your Snowflake organization name  
+TF_VAR_ACCOUNT_NAME: Your Snowflake account name 
+TF_VAR_PRIVATE_KEY_PATH: The path within the GitHub Actions runner where your private key file will be. Important: You will also need to securely store the content of your snowflake_tf_snow_key.p8 file as another secret, and then write it to this path in a workflow step before tofu init and tofu plan are run.
+A common way to handle the private key file itself is to:
+
+Create another secret, for example, SNOWFLAKE_PRIVATE_KEY, and paste the content of your snowflake_tf_snow_key.p8 file as its value.
+In your GitHub Actions workflow, add a step before Tofu Init to write the content of this secret to the path specified by TF_VAR_PRIVATE_KEY_PATH.
+Here's an example step to write the private key content to a file:
---
+++ main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "snowflakedb/snowflake"
+      version = ">= 2.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-miguel"
+    storage_account_name = "samiguel2025"
+    container_name       = "tfs"
+    key                  = "snowflake/tofu/terraform.tfstate"
+  }
+}
+
+variable "organization_name" {
+  type = string
+  # Optionally, add a description:
+  # description = "The name of your Snowflake organization."
+}
+
+variable "account_name" {
+  type = string
+  # Optionally, add a description:
+  # description = "The name of your Snowflake account."
+}
+
+variable "private_key_path" {
+  type = string
+  # Optionally, add a description:
+  # description = "The path to the private key file for Snowflake authentication."
+}
+
+locals {
+  organization_name = var.organization_name
+  account_name      = var.account_name
+  private_key_path  = var.private_key_path
+}
+
+
+provider "snowflake" {
+    organization_name = local.organization_name
+    account_name      = local.account_name
+    user              = "TERRAFORM_SVC"
+    role              = "SYSADMIN"
+    authenticator     = "SNOWFLAKE_JWT"
+    private_key       = file(local.private_key_path)
+}
+
+
+resource "snowflake_database" "dummy_from_terraform" {
+  name         = "TF_DEMO_DB"
+  is_transient = false
+}
+
+resource "snowflake_warehouse" "dummy_warehouse_from_terraform" {
+  name                      = "WH_DUMMY"
+  warehouse_type            = "STANDARD"
+  warehouse_size            = "XSMALL"
+  max_cluster_count         = 1
+  min_cluster_count         = 1
+  auto_suspend              = 60
+  auto_resume               = true
+  enable_query_acceleration = false
+  initially_suspended       = true
+}
+
